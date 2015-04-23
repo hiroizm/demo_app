7f4924e600a04139354599553cd01dad5e52407f
tsukioka <tsukioka@itrade.co.jp>
2015-04-23 17:57:27 +0900

    routes.rb modify again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'user#index'
+  root :to => 'users#index'
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
